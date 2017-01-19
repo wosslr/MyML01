d52ebe642a15ff761e41ad981863d7cb304c2438
Ray Lv <ray.lv@sap.com>
2017-01-19 17:22:07 +0800

    v1

--- MyML_test03_tree.py
+++ MyML_test03_tree.py
@@ -3,6 +3,8 @@ from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import RandomForestClassifier
+from sklearn import tree
+import pydot
 
 testdata = pandas.read_csv("test_logreg.csv", sep="\t")
 print(testdata.head(6))
@@ -13,28 +15,33 @@ print(testdata.describe())
 print(testdata.head(6))
 print(testdata.describe())
 
-predictors = ["IND1", "IND2"]
+predictors = ["IND1", "IND2", "IND3"]
 targets = ["RES"]
 
-alg = LogisticRegression(C=1e9)
+# alg = LogisticRegression(C=1e9)
 # alg = LinearRegression()
+alg = tree.DecisionTreeClassifier()
+
 alg.fit(testdata[predictors], testdata[targets])
 
-test_input = []
+tree.export_graphviz(alg, out_file="tree.dot", feature_names=predictors, class_names=["False", "True"])
+
+(graph,) = pydot.graph_from_dot_file("tree.dot")
+graph.write_png("tree.png")
 
 kf = KFold(n_splits=300, random_state=1)
 
-predictions = []
-for train, test in kf.split(testdata):
-    # print("train: ", train, "test: ", test)
-    train_predictors = testdata[predictors].iloc[train]
-    train_target = testdata["RES"].iloc[train]
-    print(train)
-    print(test)
-    alg.fit(train_predictors, train_target)
-    test_predictions = alg.predict(testdata[predictors].iloc[test,:])
-    predictions.append(test_predictions)
-    print(test_predictions[0])
+# predictions = []
+# for train, test in kf.split(testdata):
+#     # print("train: ", train, "test: ", test)
+#     train_predictors = testdata[predictors].iloc[train]
+#     train_target = testdata["RES"].iloc[train]
+#     print(train)
+#     print(test)
+#     alg.fit(train_predictors, train_target)
+#     test_predictions = alg.predict(testdata[predictors].iloc[test,:])
+#     predictions.append(test_predictions)
+#     print(test_predictions[0])
 # print(predictions)
 
 # scores = cross_val_score(alg, testdata[predictors], testdata["point"], cv=kf.split(testdata))
@@ -45,7 +52,8 @@ while True:
     test_input = []
     ind1 = raw_input("IND1: ")
     ind2 = raw_input("IND2: ")
-    test_input.append([float(ind1), float(ind2)])
+    ind3 = raw_input("IND3: ")
+    test_input.append([float(ind1), float(ind2), float(ind3)])
     test_result = alg.predict(test_input)
     print("result: ")
     print(test_result)
